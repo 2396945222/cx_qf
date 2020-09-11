0636ed5e068989800a7a4eb46074ea62bea32ca3
2396945222@qq.com <2396945222@qq.com>
2020-09-11 21:30:51 +0800

    创建项目

---
+++ cx_qf/src/main/java/com/cf/cx_qf/CxQfApplication.java
@@ -0,0 +1,13 @@
+package com.cf.cx_qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxQfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxQfApplication.class, args);
+    }
+
+}
